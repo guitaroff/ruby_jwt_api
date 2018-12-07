6b783814e171a6c20297073474eafb26fe8ef233
andrey <ag82pishi@yandex.ru>
2018-12-07 18:44:26 +0300

    session#sign_in: with jwt token

--- apps/api/controllers/authentication.rb
+++ apps/api/controllers/authentication.rb
@@ -3,20 +3,6 @@ module Api::Controllers
     def self.included(base)
       base.class_eval do
         before :logins!
-        #before :authenticate!
-        #expose :current_user
-
-        # attr_accessor :auth_handler
-      end
-    end
-
-    # def authentication_handler
-    #   self.auth_handler || Efco::Interactors::Users::JWTAuth.new
-    # end
-
-    module Skip
-      def authenticate!
-        # kek
       end
     end
 
@@ -29,23 +15,5 @@ module Api::Controllers
         user3: '123456'
       }
     end
-
-    # def authenticate!
-    #   @current_user ||= authenticate_user
-    #   render_error code: 401, status: 'unauthorized' unless !!@current_user
-    # end
-
-    # def auth_token
-    #   params.env.fetch('HTTP_AUTHORIZATION', '').sub('Bearer ', '')
-    # end
-
-    # def authenticate_user
-    #   result = authentication_handler.call(auth_token)
-    #   if result.success?
-    #     result.user
-    #   else
-    #     nil
-    #   end
-    # end
   end
 end
--- apps/api/controllers/session/sign_in.rb
+++ apps/api/controllers/session/sign_in.rb
@@ -14,44 +14,25 @@ module Api
         def call(params)
           username = params[:sign_in][:username]
           password = params[:sign_in][:password]
-          puts "PARAMS: #{params.to_h}"
-
-          #self.body = { message: "You logged in. Yay you!" }.to_json
-          # if username == password
-          #   self.body = { message: "You logged in. Yay you!" }.to_json
-          # else
-          #   halt 401
-          # end
 
           if @logins[username.to_sym] == password
-            self.body = { message: "You logged in. Yay you!" }.to_json
+            self.body = { token: token(username) }.to_json
           else
             halt 401
           end
-
-          # if @logins[username.to_sym] == password
-          #   content_type :json
-          #   { token: token(username) }.to_json
-          # else
-          #   halt 401
-          # end
         end
 
-        # def token username
-        #   JWT.encode payload(username), ENV['JWT_SECRET'], 'HS256'
-        # end
+        def token username
+          JWT.encode payload(username), ENV['JWT_SECRET'], 'HS256'
+        end
 
-        # def payload username
-        #   {
-        #     exp: Time.now.to_i + 60 * 60,
-        #     iat: Time.now.to_i,
-        #     iss: ENV['JWT_ISSUER'],
-        #     scopes: ['add_money', 'remove_money', 'view_money'],
-        #     user: {
-        #       username: username
-        #     }
-        #   }
-        # end
+        def payload username
+          {
+            exp: Time.now.to_i + 60 * 60,
+            sub: username,
+            jti: SecureRandom.uuid
+          }
+        end
       end
     end
   end
